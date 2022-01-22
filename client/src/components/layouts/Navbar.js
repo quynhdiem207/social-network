68243e45920c8db26439a354dd874b50cdf4fbb2
Diem-Quynh <Diem-Quynh@users.noreply.github.com>
2022-01-22 21:31:22 +0700

    C22 - CSS for mobile

--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -1,7 +1,8 @@
+import { useEffect, useState, useRef } from "react";
 import { Link, useNavigate } from 'react-router-dom';
-import clsx from "clsx";
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import clsx from "clsx";
 
 import { logout } from '../../actions/auth'
 import styles from '../scss/Navbar.module.scss';
@@ -9,8 +10,24 @@ import styles from '../scss/Navbar.module.scss';
 const Navbar = ({ auth: { isAuthenticated }, logout }) => {
     const navigate = useNavigate();
 
+    const [displaySubnav, toggleSubnav] = useState(false);
+    const subnav = useRef();
+
+    useEffect(() => {
+        if (displaySubnav) {
+            subnav.current.classList.remove(styles.notDisplay);
+            subnav.current.classList.add(styles.display);
+        } else {
+            subnav.current.classList.remove(styles.display);
+            subnav.current.classList.add(styles.notDisplay);
+        }
+    }, [displaySubnav]);
+
     const guestLinks = (
-        <ul>
+        <ul
+            ref={subnav}
+            className={styles.subnav}
+        >
             <li><Link to="/profiles">Developers</Link></li>
             <li><Link to="/register">Register</Link></li>
             <li><Link to="/login">Login</Link></li>
@@ -18,19 +35,20 @@ const Navbar = ({ auth: { isAuthenticated }, logout }) => {
     );
 
     const authLinks = (
-        <ul>
+        <ul
+            ref={subnav}
+            className={styles.subnav}
+        >
             <li><Link to="/profiles">Developers</Link></li>
             <li><Link to="/posts">Posts</Link></li>
             <li>
                 <Link to="/dashboard">
-                    <i className={clsx('fas', 'fa-user')} /> {' '}
-                    <span className='hide-sm'>Dashboard</span>
+                    <i className={clsx('fas', 'fa-user', 'hide-sm')} /> Dashboard
                 </Link>
             </li>
             <li>
                 <a href='#!' onClick={() => logout(navigate)}>
-                    <i className={clsx('fas', 'fa-sign-out-alt')} /> {' '}
-                    <span className='hide-sm'>Logout</span>
+                    <i className={clsx('fas', 'fa-sign-out-alt', 'hide-sm')} /> Logout
                 </a>
             </li>
         </ul>
@@ -41,7 +59,15 @@ const Navbar = ({ auth: { isAuthenticated }, logout }) => {
             <h1>
                 <Link to="/"><i className="fas fa-code"></i> DevConnector</Link>
             </h1>
-            {isAuthenticated ? authLinks : guestLinks}
+            <div>
+                <div
+                    className={styles.menuIcon}
+                    onClick={() => toggleSubnav(!displaySubnav)}
+                >
+                    <i className="fa-solid fa-bars"></i>
+                </div>
+                {isAuthenticated ? authLinks : guestLinks}
+            </div>
         </nav>
     )
 }
